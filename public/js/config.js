eaa9fc2a281f959ab0e10bce9d9aeb5738423fb0
sep.park <dytpq0978@naver.com>
2025-01-07 09:32:38 +0900

    port 접근 방식 변경 및 권한이슈 및 좋아요 증가 방식 변경

--- public/js/config.js
+++ public/js/config.js
@@ -5,13 +5,13 @@ if (typeof window !== "undefined") {
     API_URL =
         window.location.hostname === "localhost"
             ? "http://localhost:4000"
-            : "http://3.83.156.215:4000";
+            : "http://18.212.253.92:4000";
 } else {
     // Node.js 환경
     API_URL =
         process.env.NODE_ENV === "development"
             ? "http://localhost:4000"
-            : "http://3.83.156.215:4000";
+            : "http://18.212.253.92:4000";
 }
 
 export default API_URL;
